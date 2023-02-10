4d432eef785000bc6951590e5e19c2d8c646ac98
DebuggingOnProd <lhqblue@outlook.com>
2023-02-10 21:27:55 +0800

    Initial commit

---
+++ src/main/java/org/lhq/e5renew/E5renewApplication.java
@@ -0,0 +1,13 @@
+package org.lhq.e5renew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E5renewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E5renewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lhq/e5renew/E5renewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lhq.e5renew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E5renewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
