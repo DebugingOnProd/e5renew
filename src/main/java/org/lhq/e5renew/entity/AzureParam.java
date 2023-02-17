346fd5cad6375171fe53daaa11e34a0bc2c786fe
laiwenzhuo <939296345@qq.com>
2023-02-17 11:54:09 +0800

    秘钥RSA加密，动态修改cron表达式实现动态修改定时任务调度

--- src/main/java/org/lhq/e5renew/entity/AzureParam.java
+++ src/main/java/org/lhq/e5renew/entity/AzureParam.java
@@ -1,10 +1,16 @@
 package org.lhq.e5renew.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+import org.lhq.e5renew.config.anno.Sensitive;
 
 @Data
+@TableName("azure_param")
 public class AzureParam {
+    private Long id;
     private String clientId;
+
+    @Sensitive(prefixNoMaskLen = 4,suffixNoMaskLen = 4)
     private String clientSecret;
     private String tenantId;
 }
